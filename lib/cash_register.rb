566084590337e75f5d34280eab8ec776426868fe
mehmehmehlol <hkmeganlo@gmail.com>
2020-08-26 16:12:21 -0700

    add a CashRegister method

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,27 @@
+require 'pry'
 
+class CashRegister
+    attr_accessor :total, :discount, :items
+    
+
+    def initialize(discount=0)
+        @total = 0
+        @discount = discount
+        @items = []
+    end
+
+    def add_item(title, amount, quantity)
+        self.total += amount * quantity
+        quantity.times do
+            items << title
+        end
+    end 
+
+end
+
+
+binding.pry
+cash_register = CashRegister.new
+cash_register.add_item("eggs", 0.98)
+p cash_register.total
+# cash_register_with_discount = CashRegister.new(20)
